ea2014c9b3f04661fb4abcf19b9b898729e025d9
Aditya <adit.widi@gmail.com>
2024-09-24 22:18:37 +0700

    update learning looping in js

---
+++ Belajar Javascript/sangPengaturProgram.js
@@ -0,0 +1,64 @@
+function categorizeNumber(input) {
+
+    if (typeof input !== 'number') {
+
+        throw new Error("Input harus berupa bilagan bulat")
+
+    }
+
+    if (input < 0) {
+        return "Negatif"
+    } else if (input == 0) {
+        return "Nol"
+    } else if (isPrima(input)) {
+        return "Prima"
+    } else if (input%2 == 1) {
+        return "Ganjil"
+    } else if (input%2 == 0) {
+        return "Genap"
+    }
+
+
+    function isPrima(input) {
+
+        if (input <= 1) return false;
+
+        for (let i = 2; i <= Math.sqrt(input); i++) {
+
+            if (input % i === 0) {
+
+                return false;
+
+            }
+
+        }
+
+        return true;
+        /**
+         * TODO:
+         * Lengkapilah fungsi ini agar dapat menghasilkan output berupa kategori bilangan berdasarkan input yang diberikan.
+         * Dengan ketentuan sebagai berikut:
+         * 1. Jika input bukan number, bangkitkan (throw) error.
+         * 2. Jika input adalah bilangan genap, kembalikan dengan nilai "Genap".
+         * 3. Jika input adalah bilangan ganjil, kembalikan dengan nilai "Ganjil".
+         * 4. Jika input adalah bilangan prima, kembalikan dengan nilai "Prima".
+         * 5. Jika input adalah nol, kategorikan kembalikan dengan nilai "Nol".
+         * 6. Jika input negatif, kembalikan dengan nilai "Negatif".
+         */
+
+
+    }
+}
+
+// Contoh
+console.log(categorizeNumber(15)); // Output: "Ganjil"
+console.log(categorizeNumber(12)); // Output: "Genap"
+console.log(categorizeNumber(17)); // Output: "Prima"
+console.log(categorizeNumber(0)); // Output: "Nol"
+console.log(categorizeNumber(-5)); // Output: "Negatif"
+
+try {
+    categorizeNumber('abc');
+} catch (error) {
+    console.log(error.message);
+} // Output: "Input harus berupa bilangan bulat"
